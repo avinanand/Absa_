fcf37f3591b936c7a40b9758668d3a2b61a3292b
Avinash <avinashanandyadav@gmail.com>
2020-08-11 13:42:08 +0530

    Update model.py

--- AspectModel/model.py
+++ AspectModel/model.py
@@ -1,3 +1,17 @@
+#we fetch the model from the BERT_PATH defined in 
+#config.py 
+#BERT in its default settings returns two outputs 
+#last hidden state and output of bert pooler layer  
+#used the output of the pooler which is of the size  
+#(batch_size, hidden_size)  
+#hidden size can be 768 or 1024 depending on  
+#if we are using bert base or large respectively  
+#in our case, it is 768  
+#note that this model is pretty simple 
+#might want to use last hidden state 
+#or several hidden states 
+
+
 import config
 import transformers
 import torch.nn as nn
